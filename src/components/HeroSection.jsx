d3114df339f9ae0665c08c7d70924ef6fcfc27f8
Muhammad Huzaifa <huzaifazafar750@gmail.com>
2024-08-11 11:25:39 +0500

    CV

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -71,7 +71,7 @@ const HeroSection = ({ isNavbarOpen }) => {
                     </p>
                     <div className="flex flex-col sm:flex-row items-start sm:items-center space-y-4 sm:space-y-0 sm:space-x-4 mb-8 lg:mb-12 justify-center">
                         <a
-                            href='https://drive.google.com/uc?export=download&id=1oKWP6VxOQQh9woecL4Jw2mlpLGCamdEf'
+                            href='https://drive.google.com/uc?export=download&id=1OFMm2fcwUqZUvUgsI2kUsXq3ogM9Gq1W'
                             download
                             target='_blank'
                             className="bg-gray-700 text-white hover:bg-gray-500 py-2 px-6 rounded-full text-lg font-semibold transition duration-300 ease-in-out whitespace-nowrap mx-auto lg:mx-0"
